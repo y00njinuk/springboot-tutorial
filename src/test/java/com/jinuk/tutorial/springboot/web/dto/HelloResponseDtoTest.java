c9c10d93d096047b6ed8fea510261d172359f5c7
y00njinuk <jinuknamja@gmail.com>
2022-12-19 23:52:40 +0900

    feat: add HelloResponseDto and it of test
    - in Chapter02

---
+++ src/test/java/com/jinuk/tutorial/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.jinuk.tutorial.springboot.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name); // 테스트 검증 메서드
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
