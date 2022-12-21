22d5964ae6024a6e40be53cd363e5c8f77f7b3f0
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2022-12-21 23:34:02 +0900

    feat: add Posts Service & Domain & Repository Layer
    - in Chapter03

---
+++ src/main/java/com/jinuk/tutorial/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.jinuk.tutorial.springboot.service.posts;
+
+import com.jinuk.tutorial.springboot.domain.posts.PostsRepository;
+import com.jinuk.tutorial.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
