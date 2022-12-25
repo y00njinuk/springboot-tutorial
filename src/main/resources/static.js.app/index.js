57f8dd8fa023be1351c76bf63d21f6584208daeb
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2022-12-26 08:38:29 +0900

    feat: add new posts page
    - in Chapter04

---
+++ src/main/resources/static.js.app/index.js
@@ -0,0 +1,39 @@
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
+    },
+    save : function () {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            // callback 구현으로 글 등록이 성공하면 메인페이지(/)로 다시 이동
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+main.init();
