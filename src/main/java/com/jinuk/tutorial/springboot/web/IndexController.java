445d6d80932fce855cbd14b61f2d5710cb477bba
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2022-12-26 08:09:07 +0900

    feat: add IndexController
    - in Chapter04

---
+++ src/main/java/com/jinuk/tutorial/springboot/web/IndexController.java
@@ -0,0 +1,20 @@
+package com.jinuk.tutorial.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 매핑된 URL에 따라 페이지를 반환하는 컨트롤러
+ * 페이지 반환 시, 앞의 경로와 뒤의 파일 확장자는 자동으로 지정
+ * 앞의 경로 - src/main/resources/templates/
+ * 뒤의 경로 - .mustache
+ */
+public class IndexController {
+    /**
+     * index page.
+     * @return src/main/resources/templates/index.mustache
+     */
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
