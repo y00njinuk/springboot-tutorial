105af937bed137f03aa8d3e952d85d4bd8073788
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2022-12-28 07:20:59 +0900

    feat: add modify posts page
    - in Chapter04

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -5,6 +5,10 @@ var main = {
             _this.save();
         });
 
+        /*
+           btn-update id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때
+           update 함수를 호출하도록 한다.
+        */
         $('#btn-update').on('click', function () {
             _this.update();
         });
@@ -29,6 +33,27 @@ var main = {
         }).done(function() {
             // callback 구현으로 글 등록이 성공하면 메인페이지(/)로 다시 이동
             alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update : function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
