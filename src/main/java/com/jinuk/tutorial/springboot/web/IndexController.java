3a1cbb78c810258401d99024bed442090f0118db
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2022-12-28 06:59:19 +0900

    feat: add select Posts in index page
    - in Chapter04

--- src/main/java/com/jinuk/tutorial/springboot/web/IndexController.java
+++ src/main/java/com/jinuk/tutorial/springboot/web/IndexController.java
@@ -1,7 +1,9 @@
 package com.jinuk.tutorial.springboot.web;
 
+import com.jinuk.tutorial.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -13,12 +15,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
+
     /**
      * index page.
      * @return src/main/resources/templates/index.mustache
      */
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        /**
+         * Model은 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
+         * postsService.findAllDEsc()로 가져온 결과를 posts 변수로 index.mustache에 전달
+         */
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
