4df0f862d49e17e43f4ce8dda478d3af1b82797b
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2022-12-28 06:55:40 +0900

    feat: add select in PostService
    - in Chapter04

--- src/main/java/com/jinuk/tutorial/springboot/service/posts/PostsService.java
+++ src/main/java/com/jinuk/tutorial/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.jinuk.tutorial.springboot.service.posts;
 
 import com.jinuk.tutorial.springboot.domain.posts.Posts;
 import com.jinuk.tutorial.springboot.domain.posts.PostsRepository;
+import com.jinuk.tutorial.springboot.web.dto.PostsListResponseDto;
 import com.jinuk.tutorial.springboot.web.dto.PostsResponseDto;
 import com.jinuk.tutorial.springboot.web.dto.PostsSaveRequestDto;
 import com.jinuk.tutorial.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,13 @@ public class PostsService {
                 () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    // (readOnly = true)? 트랜잭션 범위는 유지하되 조회 기능만 남겨두어 조회 속도를 개선
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
