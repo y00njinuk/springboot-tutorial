034834a930ab60fce390ffb391532d1db08ece31
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-01-02 08:32:24 +0900

    feat: add Service class for Spring Security
    - in Chapter05

---
+++ src/main/java/com/jinuk/tutorial/springboot/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,68 @@
+package com.jinuk.tutorial.springboot.config.auth;
+
+import com.jinuk.tutorial.springboot.config.auth.dto.OAuthAttributes;
+import com.jinuk.tutorial.springboot.config.auth.dto.SessionUser;
+import com.jinuk.tutorial.springboot.domain.user.User;
+import com.jinuk.tutorial.springboot.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+/**
+ * 로그인 이후 가져온 사용자 정보들을 기반으로 가입 및 정보수정, 세션 저장 등의 기능을 한다.
+ */
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        // 현재 로그인 진행 중인 서비스를 구분하는 코드
+        // 추후, 구글을 포함한 다른 서비스의 로그인 연동시에 구분하기 위한 용도로 사용
+        String registrationId = userRequest
+                .getClientRegistration()
+                .getRegistrationId();
+
+        // Oauth2 로그인 진행 시 키가 되는 필드 값으로 구글의 경우 기본 코드 값은 "sub"
+        String userNameAttributeName = userRequest
+                .getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(
+                registrationId,
+                userNameAttributeName,
+                oAuth2User.getAttributes());
+
+        User user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
+    }
+}
