31cdd7c2ef306ddce066107ffabcf9c56fd80b01
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-01-02 23:02:10 +0900

    feat: add Handler and Config classes for annotation
    - in Chapter05

---
+++ src/main/java/com/jinuk/tutorial/springboot/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.jinuk.tutorial.springboot.config;
+
+import com.jinuk.tutorial.springboot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * 스프링에서 다른 클래스들이 인식될 수 있도록 WebMvcConfigurer를 구현한 클래스
+ */
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
---
+++ src/main/java/com/jinuk/tutorial/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,55 @@
+package com.jinuk.tutorial.springboot.config.auth;
+
+import com.jinuk.tutorial.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * 특정 메서드에서 파라미터의 값을 확인하고 변환할 수 있도록 HandlerMethodArgumentResolver를 구현한 클래스
+ */
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+    /**
+     * 컨트롤러 메서드의 특정 파라미터를 지원하는지 판단하는 메서드
+     *
+     * @param parameter the method parameter to check
+     * @return
+     */
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    /**
+     * 파라미터의 전달할 객체를 생성하는 메서드
+     *
+     * @param parameter     the method parameter to resolve. This parameter must
+     *                      have previously been passed to {@link #supportsParameter} which must
+     *                      have returned {@code true}.
+     * @param mavContainer  the ModelAndViewContainer for the current request
+     * @param webRequest    the current request
+     * @param binderFactory a factory for creating {@link WebDataBinder} instances
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
