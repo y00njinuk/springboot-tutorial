9d10d338629bd7117dc650bab10bd007f15308f8
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-01-04 08:53:32 +0900

    refactor: add filter for exclude SecurityConfig
    - SecurityConfig 클래스 생성 시 CustomOAuth2UserService 클래스가 필요한데 해당 클래스는 인증 정보를 필요로 하기 때문에 테스트 상에서 다음과 같은 에러가 발생하므로 filter를 생성하여 조치
    - No qualifying bean of type 'com.jinuk.tutorial.springboot.config.auth.CustomOAuth2UserService' available: expected at least 1 bean which qualifies as autowire candidate. Dependency annotations: {}
    - in Chapter05

--- src/test/java/com/jinuk/tutorial/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jinuk/tutorial/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.jinuk.tutorial.springboot.web;
 
+import com.jinuk.tutorial.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,14 +17,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class) // 테스트 진행 시 JUnit에 내장된 실행파일 외 다른 실행파일을 실행
                              // SpringRunner? 스프링부트와 Junit 사이의 다리 역할을 한다.
-@WebMvcTest(controllers = HelloController.class) // Spring MVC에 집중하는 어노테이션
-                                                 // @Controller, @ControllerAdvice 사용가능
-                                                 // @Service, @Component, @Repository 사용불가
+
+// Spring MVC에 집중하는 어노테이션
+// @Controller, @ControllerAdvice 사용가능
+// @Service, @Component, @Repository 사용불가
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {                          // SecurityConfig 클래스를 제외하기 위한 필터
+        @ComponentScan.Filter(                      // SecurityConfig 클래스는 CustomOAuth2UserService 클래스가 필요하여,
+                type = FilterType.ASSIGNABLE_TYPE,  // 해당 클래스 생성시 인증에 관한 정보가 필요하지만 테스트에서는 정보가 없으므로 에러 발생
+                classes = SecurityConfig.class)})
+
 public class HelloControllerTest {
     @Autowired           // 스프링이 관리하는 빈(Bean)을 주입
     private MockMvc mvc; // 웹 API 테스트 시 사용되며 GET,POST 등 API 테스트 가능
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -30,6 +42,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
