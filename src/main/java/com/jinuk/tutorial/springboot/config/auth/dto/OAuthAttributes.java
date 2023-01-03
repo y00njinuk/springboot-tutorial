df26dedbd383ef7d2dcee568a34c823b39138518
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-01-04 06:15:14 +0900

    feat: add User login code of Naver
    - in Chapter05

--- src/main/java/com/jinuk/tutorial/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jinuk/tutorial/springboot/config/auth/dto/OAuthAttributes.java
@@ -28,7 +28,7 @@ public class OAuthAttributes {
     }
 
     /**
-     * 구글을 통해 로그인한 사용자 정보를 활용하여 변환
+     * 로그인한 사용자 정보를 활용하여 변환
      *
      * @param registrationId
      * @param userNameAttributeName
@@ -36,7 +36,22 @@ public class OAuthAttributes {
      * @return
      */
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId))
+            return ofNaver("id", attributes);
+
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String)response.get("name"))
+                .email((String)response.get("email"))
+                .picture((String)response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
