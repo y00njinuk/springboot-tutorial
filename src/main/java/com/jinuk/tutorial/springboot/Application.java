278726978b75353e67a6b719bd7abfdb1b7c6d9d
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-01-04 09:07:58 +0900

    refactor: separate @EnableJpaAuditing from Application
    - @EnableJpaAuditing은 Entity 클래스를 필요로 하기에 @WebMvcTest를 사용한 테스트에서 다음 오류가 발생하여 이를 분리
    - JPA metamodel must not be empty!
    - in Chapter05

--- src/main/java/com/jinuk/tutorial/springboot/Application.java
+++ src/main/java/com/jinuk/tutorial/springboot/Application.java
@@ -2,9 +2,7 @@ package com.jinuk.tutorial.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 기능을 활성화
 @SpringBootApplication // 스프링 부트의 자동 설정. 스프링 Bean 읽기와 생성을 모두 자동으로 설정
 public class Application {
     public static void main(String[] args) {
