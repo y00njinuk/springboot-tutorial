04b74c8f1ad09943d7f97ba0572e30f6369c13a1
y00njinuk <jinuknamja@gmail.com>
2023-01-12 08:29:28 +0900

    feat: add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+REPOSITORY=/root/app/step1
+PROJECT_NAME=springboot-tutorial
+
+cd $REPOSITORY/$PROJECT_NAME
+
+echo "> Git Pull"
+git pull
+
+echo "> 프로젝트 build 시작"
+./gradlew build
+
+echo "> step1 디렉토리로 이동"
+cd $REPOSITORY
+
+echo "> Build 파일 복사"
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+
+echo "> 현재 구동중인 애플리케이션 pid : $CURRENT_PID"
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+
+echo "> JAR NAME : $JAR_NAME"
+nohup java -jar \
+    # 스프링의 설정 파일 위치를 지정한다.
+    # classpath는 jar 안에 있는 resources 디렉토리를 기준으로 경로가 생성된다.
+    # application-oauth.properties는 프로젝트 외부에서 관리해야 하므로 절대 경로를 사용한다.
+    -Dspring.config.location=classpath:/application.properties,/root/app/step1/application-oauth.properties \
+    $REPOSITORY/$JAR_NAME 2>&1 &
